fec034bd125312a85bc2455062d011ba3a2820ac
Prasanth Dendukuri <prasanthdendukuri@gmail.com>
2026-09-13 01:03:18 -0700

    Add draggable opposite-view panel and tab switching

--- Moneta/MonetaTheme.swift
+++ Moneta/MonetaTheme.swift
@@ -3,4 +3,5 @@ import SwiftUI
 extension Color {
     static let monetaBlack = Color(red: 8 / 255, green: 8 / 255, blue: 10 / 255)
     static let monetaPaper = Color(red: 247 / 255, green: 246 / 255, blue: 243 / 255)
+    static let monetaTertiary = Color(red: 142 / 255, green: 142 / 255, blue: 147 / 255)
 }
